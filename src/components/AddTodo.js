873a6e3e31d7fc858dd1631bcf854e032bc4db56
IMS <inisoetan2014@gmail.com>
2022-12-14 10:03:34 +0000

    Implement add todo list item 2

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import '../styles/AddTodo.css'
+import uuid from 'react-uuid'
 export default function AddTodo(props) {
+    function handleTodoInput(e) {
+        const newValue = e.target.value
+        console.log(newValue)
+        props.setTodoInput(newValue)
+    }
+
+    function addItem(e) {
+        e.preventDefault()
+        const newItem = { id: uuid(), value: props.todoInput, isDone: false }
+        props.setTodoItems(prevItems => [...prevItems, newItem])
+        console.log(props.todoItems)
+        props.setTodoInput("")
+    }
     return (
             <form className="todo-input">
-                <input type="text" placeholder="Add a new task" value={props.val} onChange={props.handleChange}>
+                <input type="text" placeholder="Add a new task" value={props.val} onChange={handleTodoInput}>
                 </input>
-                <button type="submit" className="add-btn" onClick={props.add}>Add</button>
+                <button type="submit" className="add-btn" onClick={addItem}>Add</button>
             </form>
     )
 }
