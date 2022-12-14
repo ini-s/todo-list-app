e26e6245ee646004df6a5a7dd0103d6c257826c6
IMS <inisoetan2014@gmail.com>
2022-12-14 15:22:18 +0000

    Implement add todo list item 6

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ export default function App() {
             <Header />
             <main>
                 <AddTodo
-                    todoItems={todoItems}
                     setTodoItems={setTodoItems}
                 />
                 <Todo />
