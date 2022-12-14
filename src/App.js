124a3be66275ba41ca22635d020700d990e85fdc
IMS <inisoetan2014@gmail.com>
2022-12-14 09:33:20 +0000

    Implement add todo list item

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './components/Header'
 import AddTodo from './components/AddTodo'
 import Todo from './components/Todo'
 import Footer from './components/Footer'
 import './App.css'
-export default function App (){
+import uuid from 'react-uuid'
+export default function App() {
+    const [todoItems, setTodoItems] = useState([])
+    const [todoInput, setTodoInput] = useState("")
+
+    function handleTodoInput(e) {
+        const newValue = e.target.value
+        console.log(newValue)
+        setTodoInput(newValue)
+    }
+
+    function addItem(e) {
+        e.preventDefault()
+        const newItem = { id: uuid(), value: todoInput, isDone: false }
+        setTodoItems(prevItems => [...prevItems, newItem])
+        console.log(todoItems)
+        setTodoInput("")
+    }
     return (
         <div className="App">
             <Header />
             <main>
-                <AddTodo/>
+                <AddTodo
+                    val={todoInput}
+                    handleChange={handleTodoInput}
+                    add={addItem}
+                />
                 <Todo />
             </main>
             <Footer />
--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import '../styles/AddTodo.css'
-export default function AddTodo() {
+export default function AddTodo(props) {
     return (
             <form className="todo-input">
-                <input type="text" placeholder="Add a new task">
+                <input type="text" placeholder="Add a new task" value={props.val} onChange={props.handleChange}>
                 </input>
-                <button type="submit" className="add-btn">Add</button>
+                <button type="submit" className="add-btn" onClick={props.add}>Add</button>
             </form>
     )
 }
