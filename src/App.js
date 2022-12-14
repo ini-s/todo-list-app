785221b1fc85a9c738c6c7f32b938eecc3658a9c
IMS <inisoetan2014@gmail.com>
2022-12-14 10:10:37 +0000

    Implement add todo list item 3

--- src/App.js
+++ src/App.js
@@ -6,18 +6,14 @@ import Footer from './components/Footer'
 import './App.css'
 export default function App() {
     const [todoItems, setTodoItems] = useState([])
-    const [todoInput, setTodoInput] = useState("")
 
     return (
         <div className="App">
             <Header />
             <main>
                 <AddTodo
-                    val={todoInput}
                     todoItems={todoItems}
-                    todoInput={todoInput}
                     setTodoItems={setTodoItems}
-                    setTodoInput={setTodoInput}
                 />
                 <Todo />
             </main>
--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,25 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/AddTodo.css'
 import uuid from 'react-uuid'
 export default function AddTodo(props) {
+    const [todoInput, setTodoInput] = useState("")
+
     function handleTodoInput(e) {
         const newValue = e.target.value
         console.log(newValue)
-        props.setTodoInput(newValue)
+        setTodoInput(newValue)
     }
 
     function addItem(e) {
         e.preventDefault()
-        const newItem = { id: uuid(), value: props.todoInput, isDone: false }
+        const newItem = { id: uuid(), value: todoInput, isDone: false }
         props.setTodoItems(prevItems => [...prevItems, newItem])
         console.log(props.todoItems)
-        props.setTodoInput("")
+        setTodoInput("")
     }
     return (
-            <form className="todo-input">
-                <input type="text" placeholder="Add a new task" value={props.val} onChange={handleTodoInput}>
-                </input>
-                <button type="submit" className="add-btn" onClick={addItem}>Add</button>
-            </form>
+        <form className="todo-input">
+            <input type="text" placeholder="Add a new task" value={todoInput} onChange={handleTodoInput}>
+            </input>
+            <button type="submit" className="add-btn" onClick={addItem}>Add</button>
+        </form>
     )
 }
