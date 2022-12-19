f0e716a8a5d8b7b52fd7a08d921e928351fff6cb
IMS <inisoetan2014@gmail.com>
2022-12-19 09:01:54 +0000

    Implement edit/update todo list item 2

--- src/App.js
+++ src/App.js
@@ -4,42 +4,14 @@ import AddTodo from './components/AddTodo'
 import Todo from './components/Todo'
 import Footer from './components/Footer'
 import './App.css'
-import uuid from 'react-uuid'
+
 
 export default function App() {
     const [todoItems, setTodoItems] = useState([])
-    const [todoInput, setTodoInput] = useState("")
     const [editId, setEditId] = useState()
 
-    function handleTodoInput(e) {
-        const newValue = e.target.value
-        console.log(newValue)
-        setTodoInput(newValue)
-    }
-
-    function addItem(e) {
-        e.preventDefault()
-        if (!todoInput) return
-        if (editId) {
-            const editedItem = todoInput
-            setTodoItems(prevItems => prevItems.map(item => {
-                return editId === item.id ? { ...item, value: editedItem } : item
-            }))
-            setTodoInput("")
-            setEditId()
-        }
-        else{
-            const newItem = { id: uuid(), value: todoInput, isDone: false }
-            setTodoItems(prevItems => [...prevItems, newItem])
-            setTodoInput("")
-        }
-    }
-
-    function handleEdit(id) {
-        const editItem = todoItems.find(item => item.id === id)
-        console.log(editItem)
-        setEditId(editItem.id)
-        setTodoInput(editItem.value)
+    function editTodo(id) {
+        setEditId(id)
     }
 
     return (
@@ -47,14 +19,15 @@ export default function App() {
             <Header />
             <main>
                 <AddTodo
-                    todoInput={todoInput}
-                    handleTodoInput={handleTodoInput}
-                    addItem={addItem}
+                    todoItems={todoItems}
+                    setTodoItems={setTodoItems}
                     editId={editId}
+                    setEditId={setEditId}
+
                 />
                 <Todo
                     todoItems={todoItems}
-                    handleEdit={handleEdit}
+                    handleEdit={editTodo}
                 />
             </main>
             <Footer />
--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,7 +1,44 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import '../styles/AddTodo.css'
-export default function AddTodo({ todoInput, handleTodoInput, addItem, editId }) {
+import uuid from 'react-uuid'
+export default function AddTodo({todoItems, setTodoItems, editId, setEditId}) {
+    
+    const [todoInput, setTodoInput] = useState("")
 
+    useEffect(()=>{
+        if (editId){
+            const editItem = todoItems.find(item => item.id === editId)
+            console.log(editItem)
+            setTodoInput(editItem.value)
+        }
+    }, [editId,todoItems])
+
+    function handleTodoInput(e) {
+        const newValue = e.target.value
+        console.log(newValue)
+        setTodoInput(newValue)
+    }
+
+    function addItem(e) {
+        e.preventDefault()
+        if (!todoInput) return
+        if (editId) {
+            const editedItem = todoInput
+            setTodoItems(prevItems => prevItems.map(item => {
+                return editId === item.id ? { ...item, value: editedItem } : item
+            }))
+            setTodoInput("")
+            setEditId()
+        }
+        else {
+            const newItem = { id: uuid(), value: todoInput, isDone: false }
+            setTodoItems(prevItems => [...prevItems, newItem])
+            setTodoInput("")
+        }
+    }
+
+    
+    
     return (
         <form className="todo-input">
             <input type="text" placeholder="Add a new task" value={todoInput} onChange={handleTodoInput}>
